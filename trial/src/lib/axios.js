9018ee95e5f2b6ce80dd931ed7089ab31c05752e
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-02-04 01:04:49 +0200

    env

--- trial/src/lib/axios.js
+++ trial/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://draw-contest.vercel.app/",
 });
 
 api.interceptors.request.use((config) => {
@@ -13,6 +13,10 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
+// https://draw-contest.vercel.app/
+
+// http://localhost:5000
 
 // Auth
 
