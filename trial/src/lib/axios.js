ef0621b394893e35c2ac4778ea6b94167da74723
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-02-20 07:25:30 +0200

    redirect the axios and change the title and icon

--- trial/src/lib/axios.js
+++ trial/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://api.om-elnour.art",
 });
 
 api.interceptors.request.use((config) => {
